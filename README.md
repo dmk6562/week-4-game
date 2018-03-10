0e939b6097f85a1d3159bbf153d529767b8e44d5
Catherine Muthoni <cdmuthoni2005@yahoo.com>
2018-03-09 22:57:46 -0500

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # week-4-game
+Instructions
+You will be shown a random number at the start of the game and when the game restarts.
+You win the game by matching your total score to the random number.
+You lose the game if your total score goes above the random number.
+The values of each crystal changes when the game restarts. 
+The amount of points each crystal adds is not shown to the player, but their total score is displayed and updated after each crystal is clicked.
+The number of games won and lost will be displayed.          
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,92 +1,103 @@
 //Declaring variables
-var targetNum =[];
-var crystalNum=[];
-
-//Question: Can I be able to import the container of images and assign to crystals array list? It seems to NOT work.
-var crystals = $('#crystal-imgs');
-var crystals = $('["crystal-imgs"]')
 var userScore = 0;
-var userGuesses;
-var wins=0;
-var losses=0;
+var counter = 0;
+var targetNum;
+var crystalNum =[];
+var crystalImages = $('#crystals');
+var gameActive = true;
 
-//game reset
- function reset() {
-userScore=0;
- }
-var resetAndStart = function () {
- 	$(".crystals").empty();
- 	console.log("reset");
- }
 
-//assign random target number between 19-120 
-targetNum=Math.floor((Math.random() *100) + 19);
- $(".value").text(targetNum);
- $("#yourscore").text(userScore);
+// $('#targetNumberShow').text(targetNum);
 
+// function resetGame() {
+//     gameActive =true;
+//     counter = 0;
+//     targetNum = 0;
+//     crystalNum = [];
+//     crystalValue = 0;
+//     crystalImages.unbind('click');
+//     $('#crystalsCountershow').text('--');
+//     $("#win").delay("slow").animate({opacity: "0"});
+//     $("#loss").delay("slow").animate({opacity: "0"});
+//     console.log('game reset');
+//     gameplay();
+// };
+
+function game() {
+if (gameActive = true) {
+    //assign random target number between 19-120 
+targetNum = (Math.floor(Math.random() * 100)+19);
+console.log('targetNum', 'targetNum');
 
 //assign random number to crystals between 1-12
- for (i = 0; i < 4; i++) {
-   crystalNum[i] = Math.floor(Math.random() * 12 + 1);
- } console.log("hey", crystalNum);
- console.log("Crystal Object "+crystals);
-
-//when clicked, adds number to the userScore 
- $("#blue").attr("value", crystalNum[0]);
- $("#green").attr("value", crystalNum[1]);
- $("#red").attr("value", crystalNum[2]);
- $("#yellow").attr("value", crystalNum[3]);
-
-//click function for crystals
- $("#crystal-container").on("click", ".crystal-imgs", function() {
- console.log('crystal Value', crystalNum);
-
-crystalNum = ($(this).attr("value"));
-crystalNum = parseInt(crystalNum);
-console.log('crystal Value', crystalNum);
-  
-userScore += crystalNum;
-console.log('Score', userScore);
-$(".target update").text(targetNum);
-$("#userScore update").text(userScore);
-
-if userScore = targetNum, then win
-if (userScore === targetNum) {
-alert("You win!");
- }
-
-if userScore > targetNum, then lose
-else if (userScore > targetNum) {
-   alert("You lose!!");
-  
-   }
-  
+for (i = 0; i < 4; i++) {
+    crystalNum[i] = (Math.floor(Math.random() * 12)+1);
+    console.log('crystalNum', 'crystalNum');
+    $('#targetNumberShow').text(targetNum);
 
 
+//assigning crystalNum to crystals 
+ $("#bluecrystal").attr("value", crystalNum[0]);
+ $("#greencrystal").attr("value", crystalNum[1]);
+ $("#redcrystal").attr("value", crystalNum[2]);
+ $("#yellowcrystal").attr("value", crystalNum[3]);
+ 
+
+ //when clicked, adds crystalnumbers to the userScore 
+    crystalImages.on('click', '.crystal-imgs', function() {
+    crystalValue = ($(this).attr("value"));
+    crystalValue = parseInt(crystalValue);
+    console.log('crystalValue:', 'crystalValue');
+
+    userScore += crystalValue;
+    $('#targetNumberShow').text(targetNum);
+    $('#userScoreshow').text(userScore);
+
+    //  counter += crystalValue;
+    //  console.log('counter', 'counter');
+    //  $('#crystalsCountershow').text(counter);
 
 
+     if (userScore === targetNum) {
+         alert("You Win!");
+         $("#win").animate({ opacity: "1"});
+    //  $('#userScoreshow').text(userScore);
+        (gameActive = false);
+         resetGame();
 
+     } else if (userScore > targetNum) {
+        userScore--;
+        alert("You Lost!");
+        $("#loss").animate({ opacity: "1"});
+        (gameActive= false)
+        resetGame();
+             
+    }
+     
+ });
+};
 
+ }   else {
+     resetGame();
+ };
+};
 
+game();
 
-
-
-
-        
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
+//game reset
+    function resetGame() {
+    gameActive =true;
+    userScore = 0;
+    targetNum = 0;
+    crystalNum = [];
+    crystalValue = 0;
+    crystalImages.unbind('click');
+    gameplay();
+    $('#targetNumberShow').text(targetNum);
+    $('#userScoreshow').text(userScore);
+};
+    
+ 
 
 
 
